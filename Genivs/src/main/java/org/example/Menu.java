463301658807a41fb20cdbfe43d6cc2cf6ed8f21
shahriyar moqimi <moqimishahriyar@gmail.com>
2025-04-23 20:51:36 +0330

    menu added

--- Genivs/src/main/java/org/example/Menu.java
+++ Genivs/src/main/java/org/example/Menu.java
@@ -1,5 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Menu {
+
+
+    public static void menu() {
+
+        System.out.println("****GENIVS****");
+        System.out.println("                            ");
+        System.out.println("  1. Login");
+        System.out.println("  2. Register");
+        System.out.println("  3. Exit");
+
+
+
+    }
 
 }
